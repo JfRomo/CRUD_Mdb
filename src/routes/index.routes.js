fa63a58392a962f435fd8a52adcf19efc7b21db5
JfRomo <jfromo98@gmail.com>
2022-04-28 15:19:27 -0500

    Posibilidad de guardar tareas, listar y separación de código

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,18 +3,23 @@ import Task from "../models/Task";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.render('index.hbs');
+router.get("/", async (req, res) => {
+  const tasks = await Task.find().lean()
+  res.render('index', { tasks: tasks });
 });
 
 router.get("/about", (req, res) => {
-  res.render('about.hbs');
+  res.render('about');
 });
 
-router.post('/tasks/add', (req, res) => {
-  const task = Task(req.body)
-  console.log(task)
-  res.send("Añadida")
+router.post('/tasks/add', async (req, res) => {
+  try {
+    const task = Task(req.body)
+    await task.save()
+    res.redirect('/')
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 export default router;
