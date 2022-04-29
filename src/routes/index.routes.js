d8c45ab4b974e86fa0b06e64a766f7064c1c701b
JfRomo <jfromo98@gmail.com>
2022-04-29 16:54:43 -0500

    Creadas operaciones de tareas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,22 +4,54 @@ import Task from "../models/Task";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const tasks = await Task.find().lean()
-  res.render('index', { tasks: tasks });
+  const tasks = await Task.find().lean();
+  res.render("index", { tasks: tasks });
 });
 
 router.get("/about", (req, res) => {
-  res.render('about');
+  res.render("about");
 });
 
-router.post('/tasks/add', async (req, res) => {
+router.get("/edit/:id", async (req, res) => {
   try {
-    const task = Task(req.body)
-    await task.save()
-    res.redirect('/')
+    const task = await Task.findById(req.params.id).lean()
+    res.render("edit", { task });
   } catch (error) {
-    console.log(error)
+    console.log(error.message)
   }
-})
+  
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const {id } = req.params
+  await Task.findByIdAndUpdate(id, req.body)
+  res.redirect("/");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const {id } = req.params
+  await Task.findByIdAndDelete(id)
+  res.redirect("/");
+});
+
+router.get("/toggleDone/:id", async (req, res) => {
+  const {id } = req.params
+  const task = await Task.findById(id);
+  console.log(task)
+  task.done = !task.done;
+  console.log(task)
+  await task.save();
+  res.redirect("/");
+});
+
+router.post("/tasks/add", async (req, res) => {
+  try {
+    const task = Task(req.body);
+    await task.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export default router;
